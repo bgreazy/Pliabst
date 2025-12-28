f90269c13e714a1b53d5c1ead1d7f52a1fc0e3c4
matt williams <bgreazy@msn.com>
2025-12-28 14:53:50 -0500

    init

---
+++ Source/Pliabst/Pliabst.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pliabst.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pliabst, "Pliabst" );
